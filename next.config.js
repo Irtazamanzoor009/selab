6d7a48955eb64dad9f49c823b24f03c1245fdff7
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-11-30 16:12:04 +0500

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enables static export mode
-  };
-  
-  module.exports = nextConfig;
-  
+  output: 'export', // Enables static export mode
+  images: {
+    unoptimized: true, // Ensures images work on static builds
+  },
+};
+
+module.exports = nextConfig;
