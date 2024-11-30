257b4d5f5d26f6cf7d5a4be974d75eda26a2e0a4
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-11-30 17:14:06 +0500

    ad meta tag

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import Head from 'next/head';
 import { getPostData } from '../lib/posts';
 
 export default function Home({ data, content }) {
   return (
     <div>
+      <Head>
+        <title>{data.title}</title>
+        <meta name="description" content={data.title} />
+        <meta property="og:title" content={data.title} />
+        <meta property="og:description" content={data.date} />
+        
+      </Head>
       <h1>{data.title}</h1>
       <p>{data.date}</p>
       <div dangerouslySetInnerHTML={{ __html: content }} />
