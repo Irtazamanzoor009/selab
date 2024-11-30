ac4c091f98325675f72e4bf6b6d4684eb26c8a20
Irtaza Manzoor <irtaza.manzoor1203@gmail.com>
2024-11-30 16:06:19 +0500

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { getPostData } from '../lib/posts';
+
+export default function Home({ data, content }) {
+  return (
+    <div>
+      <h1>{data.title}</h1>
+      <p>{data.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const { data, content } = getPostData();
+  return {
+    props: {
+      data,
+      content,
+    },
+  };
+}
